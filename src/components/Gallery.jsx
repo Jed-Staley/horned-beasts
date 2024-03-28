67d6a34d92fa135c55a3a3b6b05add64dba5a179
Jed Staley <JNStaley1@gmail.com>
2024-03-28 09:36:59 -0400

    Created modal for displaying a selected beast

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,28 +1,19 @@
-import { useState } from 'react';
-import beasts from '../assets/beasts.json'
+import React, { useState } from 'react';
+import beasts from '../assets/beasts.json';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
 
-function loadBeasts() {
-  const beastComponents = beasts.map((beast, index) => (
-    <section key={index}>
-      {HornedBeast(beast.title, beast.image_url, beast.description)}
-    </section>
-  ));
-  return beastComponents;
-}
-
-function HornedBeast(title, imageURL, description) {
+function RenderBeast({title, imageURL, description, onClick}) {
   const [favCount, setFavCount] = useState(0);
 
   const increaseFavs = () => setFavCount(favCount + 1);
 
   return (
-    <Card style={{ width: '18rem', margin: '10px'}}>
-      <Card.Img variant="top" src={imageURL} />
+    <Card>
+      <Card.Img variant="top" src={imageURL} alt='Horned Beast Image' onClick={onClick} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Card.Text>{description}</Card.Text>
         <Button onClick={increaseFavs} variant="primary">❤️ : {favCount}</Button>
       </Card.Body>
     </Card>
@@ -30,9 +21,46 @@ function HornedBeast(title, imageURL, description) {
 }
 
 function Gallery() {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedBeast, setSelectedBeast] = useState(null);
+
+  const openModal = (beast) => {
+    setSelectedBeast(beast);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setSelectedBeast(null);
+    setShowModal(false);
+  };
+
+  const loadBeasts = () => {
+    return beasts.map((beast, index) => (
+      <section className='beastCard' key={index}>
+        <RenderBeast
+          title={beast.title}
+          imageURL={beast.image_url}
+          description={beast.description}
+          onClick={() => openModal(beast)}
+        />
+      </section>
+    ));
+  };
+
   return (
-    <main>
-      {loadBeasts()}
+    <main className='main'>
+      <div className="grid-container">{loadBeasts()}</div>
+      {selectedBeast && (
+        <Modal show={showModal} onHide={closeModal} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>{selectedBeast.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img src={selectedBeast.image_url} alt={selectedBeast.title} style={{ maxWidth: '100%' }} />
+            <p>{selectedBeast.description}</p>
+          </Modal.Body>
+        </Modal>
+      )}
     </main>
   )
 }
